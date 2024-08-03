76726a5eb912cd51345b40506ce56c2ee6dc0e54
zzct666 <3361831042@qq.com>
2024-08-03 15:20:43 +0800

    feat：新建senDir函数，以html表格形式发送目录和文件信息给客户端

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <strings.h>
 #include <stdio.h>
+#include <dirent.h>
 
 int initListenFd(unsigned short port)
 {
@@ -218,8 +219,8 @@ int parseRequestLine(int cfd, const char* reqLine)
 	//4.客户端请求的名字是一个目录，遍历目录，发送目录内容给客户端
 	if (S_ISDIR(st.st_mode)) {
 		//遍历目录，把目录的内容发送给客户端
-		//sendHeadMsg();
-		//sendDir();	//<table></table>
+		sendHeadMsg(cfd, 200, "OK", getFileType(".html"), -1);
+		sendDir(cfd, file);	//<table></table>
 	}
 
 	//5.客户端请求的名字是一个文件，发送文件内容给客户端
@@ -279,6 +280,62 @@ int sendFile(int cfd, const char* filename)
 			return -1;
 		}
 	}
+	return 0;
+}
+
+/*
+	客户端访问目录，服务器需要遍历当前目录，并且将目录中的所有文件名发送给客户端即可
+	- 遍历目录得到的文件名需要放到html表格中
+	- 回复的数据是html格式的数据块
+	<html>
+		<head>
+			<title>test</title>
+		</head>
+		<body>
+			<table>
+				<tr>
+					<td>文件名</td>
+					<td>文件大小</td>
+				</tr>
+			</table>
+		</body>
+	<html>
+*/
+// opendir readdir closedir
+int sendDir(int cfd, const char* dirName)
+{
+	char buf[4096];
+	struct dirent** namelist;
+	sprintf(buf, "<html><head><title>%s</title></head></body><table>", dirName);
+	int num = scandir(dirName, &namelist, NULL, alphasort);
+	for (int i = 0; i < num; ++i) {
+		//取出文件名
+		char* name = namelist[i]->d_name;
+		//拼接当前文件在资源文件中的相对路径
+		char subpath[1024];
+		sprintf(subpath, "%s/%s", dirName, name);
+		struct stat st;
+		//stat函数的第一个参数是文件的路径	
+		int ret = stat(subpath, &st);
+		if (ret == -1) {
+			sendHeadMsg(cfd, 404, "Not Found", getFileType(".jpg"), -1);	// -1:大小不知道，客户端自己计算
+			sendFile(cfd, "404.jpg");
+		}
+		sprintf(buf + strlen(buf),
+			"<tr><td>%s</td><td>%ld</td></tr>", name, (long)st.st_size);
+
+		//发送数据
+		send(cfd, buf, strlen(buf), 0);
+		//清空数组
+		memset(buf, 0, sizeof(buf));
+		//释放资源	namelist[i] 这个指针指向一块有效的内存
+		free(namelist[i]);
+	}
+	//补全html剩余的标签
+	sprintf(buf, "</table></body></html>");
+	send(cfd, buf, strlen(buf), 0);
+	//释放namelist
+	free(namelist);
 	return 0;
 }
 
