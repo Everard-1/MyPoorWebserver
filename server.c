cddd955f571fa63b590c8d4439682d2acf470ec0
zzct666 <3361831042@qq.com>
2024-08-03 10:28:40 +0800

    feat：实现sendHeadMsg函数发送http头部信息

--- server.c
+++ server.c
@@ -231,16 +231,20 @@ int parseRequestLine(const char* reqLine)
 	return 0;
 }
 
-int sendHeadMsg(int cfd)
+//status：状态码     descr：状态描述	type：Content-Type的值		ConTent-Length的值
+int sendHeadMsg(int cfd, int status, const char* descr, const char* type, int Length)
 {
 	//状态行 + 消息报头 +空行
-	char buf[4096];
+	char buf[4096];		//初始化缓冲区
 	// http/1.1 200 ok
+	sprintf("http/1.1 %d %s\r\n", status, descr);
 	//消息报头 --->2个键值对
 	//conten-type：xxx   -----》https://tool.oschina.net/commons
 	// mp3--->audio/mp3
+	sprintf(buf + strlen(buf), "Content-Type: %s\r\n", type);
 	//content-length：111
 	//空行
+	sprintf(buf + strlen(buf), "Content-Length: %d\r\n\r\n", Length);
 	//拼接完成之后，发送
 	send(cfd, buf, strlen(buf),0);
 	return 0;
